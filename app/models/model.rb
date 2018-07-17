6445fe0c3048e47392bdd8c37e9e71a5dcf75ee2
LyllianT <lilyterrific2008@gmail.com>
2018-07-17 23:17:17 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,7 @@
-class Model
-  # Replace with Dog clas
+
+  #THIS IS WHERE DA RUBY HAPPENS.
+def get_fortune
+  fortune_array = ["You will step into a pothole and find a lucky penny!", "Someone will refer to a vine and you won't get it.", "Your next meal will consist of calcium, sugar, and pepper.", "No one will speak to you for a day.", "Your future will arrive soon.", "You might listen to a long lecture."]
+  return fortune_array.sample
 end
+puts get_fortune
